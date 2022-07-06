9e3cf847af5d9a5d753d9b17d84334a8c1b868c3
OlehKhoroshko <khoroshkomail@gmail.com>
2022-07-06 15:34:54 +0300

    Added: carousel, 3 sections, scss-format

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 $('.close').click(function (){
     $('.notification').slideUp(500)
 })
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', ' ' ],
+
+    responsive:{
+        0:{
+            items:1
+        },
+
+        1000:{
+            items:5
+        }
+    }
+});
